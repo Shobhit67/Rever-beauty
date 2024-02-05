603fe2ecfac2d9bb0d1a38b365f2d84fa3c0d8b5
Shobhit goyal <armyshobhit67@gmail.com>
2024-02-05 15:16:12 +0530

    Adding content

---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,62 @@
+import { CiShoppingCart } from "react-icons/ci";
+import { CiSearch } from "react-icons/ci";
+import { CiHeart, CiUser  } from "react-icons/ci";
+import logoName from "../images/logo-name.jpeg";
+
+function Navbar() {
+  return (
+    <nav className="shadow-lg p-4">
+      <div className="container mx-auto flex justify-between items-center ">
+        {/* Hamburger icon */}
+        <div className="flex items-center ">
+          <div className="flex items-center ">
+            <button className="block focus:outline-none">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-6 w-6"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path strokeWidth="2" d="M4 6h16M4 12h16m-7 6h7"></path>
+              </svg>
+            </button>
+          </div>
+        </div>
+
+        {/* Logo */}
+        <div className="flex items-center ">
+           <a href="#"><img src={logoName} alt="Logo" className="h-14" /></a>
+        </div>
+
+        {/* Navigation links */}
+        <ul className="flex space-x-8 justify-between font-normal ">
+          <li>
+            <a href="#">
+              <CiSearch  className="text-2xl"/>
+            </a>
+          </li>
+          <li>
+            <a href="#">
+              <CiHeart className="text-2xl" />
+
+            </a>
+          </li>
+          <li>
+            <a href="#">
+              <CiUser className="text-2xl" />
+
+            </a>
+          </li>
+          <li>
+            <a href="#">
+              <CiShoppingCart className="text-2xl" />
+            </a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
